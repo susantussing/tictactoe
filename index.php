bebae1726dc296d3c2d8ceb49a05966e32380e80
Susan Tussing <susan@fionavar.net>
2016-06-27 18:49:19 -0500

    Implement grid and alternating players

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  
+<?php
+
+  function display_box($grid, $i, $player) {
+    switch ($grid[$i]) {
+      case 1:
+        $content = "X";
+        break;
+      case 2:
+        $content = "O";
+        break;
+      default:
+        $content = "";
+        break;
+    }
+    $newplayer = ($player == 1) ? 2 : 1;
+    $newgrid = substr_replace($grid, $player, $i, 1);
+    echo <<<HTML
+      <a class="box" href="?grid=$newgrid&player=$newplayer">$content</a>
+HTML;
+  }
+  parse_str($_SERVER['QUERY_STRING'], $query);
+  $grid = isset($query['grid']) ? $query['grid'] : "000000000";
+  $player = isset($query['player']) ? $query['player'] : 1;
+
+
+
+  for ($i = 0; $i < 9; $i++) {
+    display_box($grid, $i, $player);
+  }
+
+
+
+?>
+<div style="clear:both;"><a href="index.php">Reset</a></div>
+
+
+</body>
+</html>
