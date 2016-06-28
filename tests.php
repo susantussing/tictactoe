b1bd7e2b1ab527c4eafa895169134e8c6ee10a9c
Susan Tussing <susan@fionavar.net>
2016-06-28 13:29:07 -0500

    Tests for functions

---
+++ tests.php
@@ -0,0 +1,28 @@
+<?php
+  include 'functions.php';
+
+  echo "Test:  Player 1 is X, player 2 is O.\n";
+
+  if (num2mark(1) == "X" && num2mark(2) == "O") {
+    echo "Success!\n";
+  } else {
+    echo "Failure.\n";
+  }
+
+  echo "Test:  In a grid of 000100200, squares 3 and 6 are taken.\n";
+
+  $grid = "000100200";
+  if (!is_available($grid, 3) && !is_available($grid, 6)) {
+    echo "Success!\n";
+  } else {
+    echo "Failure.\n";
+  }
+
+  echo "Test:  The grid 000000111 contains a winner.  The grid 000002220 does not.\n";
+  if (check_winner("000000111") == "X" && check_winner("000002220") == "") {
+    echo "Success!\n";
+  } else {
+    echo "Failure.\n";
+  }
+
+?>
