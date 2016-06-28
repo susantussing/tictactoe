901315e2c3bebc3ca247f683a5789fd8d365a9a4
Susan Tussing <susan@fionavar.net>
2016-06-27 20:22:48 -0500

    Add win checking

--- index.php
+++ index.php
@@ -11,24 +11,30 @@
 <div class="grid">
 <?php
 
-  function display_box($grid, $i, $player) {
-    switch ($grid[$i]) {
+  function num2mark ($num) {
+    switch ($num) {
       case 1:
-        $content = "X";
+        return "X";
         break;
       case 2:
-        $content = "O";
+        return "O";
         break;
       default:
-        $content = "";
+        return "";
         break;
     }
+  }
+
+  function display_box($grid, $i, $player) {
+    $content = num2mark($grid[$i]);
+
     $newplayer = ($player == 1) ? 2 : 1;
     $newgrid = substr_replace($grid, $player, $i, 1);
     echo <<<HTML
       <a class="box" href="?grid=$newgrid&player=$newplayer">$content</a>
 HTML;
   }
+
   parse_str($_SERVER['QUERY_STRING'], $query);
   $grid = isset($query['grid']) ? $query['grid'] : "000000000";
   $player = isset($query['player']) ? $query['player'] : 1;
@@ -39,7 +45,14 @@ HTML;
     display_box($grid, $i, $player);
   }
 
+  $wins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
+  foreach ($wins as $win) {
+    if ($grid[$win[0]] == $grid[$win[1]] && $grid[$win[0]] == $grid[$win[2]] && $grid[$win[0]] != 0) {
+      $winner = num2mark($grid[$win[0]]);
+      echo "$winner wins!";
+    }
+  }
 
 ?></div>
 <a href="index.php" class="reset">Reset</a>
