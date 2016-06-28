4e6f14b55d03062f6fd60127eff0bf1ad9c5c4d5
Susan Tussing <susan@fionavar.net>
2016-06-28 13:42:29 -0500

    Give the page a title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <title>Tic-Tac-Toe</title>
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/main.css">
 </head>
