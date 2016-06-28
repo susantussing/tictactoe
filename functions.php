7e0046171daf87202cead1ee32ba1e0f0379c14f
Susan Tussing <susan@fionavar.net>
2016-06-28 13:28:57 -0500

    Cleanup and comments to explain functions

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php 
 
   function num2mark ($num) {
+    // Converts a numerical grid square assignment to its appropriate mark.
+
     switch ($num) {
       case 1:
         return "X";
@@ -15,38 +17,58 @@
   }
 
   function is_available($grid, $square) {
+    // Can this player mark this square?
     return $grid[$square] == 0;
   }
 
-  function display_box($grid, $square, $player) {
+  function display_square($grid, $square, $player) {
+    // Create the HTML for a given grid square.
+
+    // The letter to put in.
     $content = num2mark($grid[$square]);
 
-    $newplayer = ($player == 1) ? 2 : 1;
-    $newgrid = substr_replace($grid, $player, $square, 1);
+    // If the square's free...
     if (is_available($grid, $square)) {
+      // If we're currently player 1, next move is player 2.
+      // Otherwise, we're player 2, and next move is player 1.
+      $newplayer = ($player == 1) ? 2 : 1;
+
+      // This square's link should go to a grid that has this player's
+      // mark in this square.
+      $newgrid = substr_replace($grid, $player, $square, 1);
+
+      // Render the HTML tag for a linked square.
       echo <<<HTML
-        <a class="box" href="?grid=$newgrid&player=$newplayer">$content</a>
+        <a class="square" href="?grid=$newgrid&player=$newplayer">$content</a>
 HTML;
     } else {
-        echo <<<HTML
-          <div class="box">$content</div>   
+      // If the square isn't free, then make an unlinked box with the 
+      // current mark.
+      echo <<<HTML
+        <div class="square">$content</div>   
 HTML;
     }
   }
 
   function display_grid($grid, $player) {
+    // Create the HTML to display the tic-tac-toe grid.
     for ($i = 0; $i < 9; $i++) {
-      display_box($grid, $i, $player);
+      display_square($grid, $i, $player);
     }
   }
 
   function check_winner($grid) {
-        $wins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+    // Check the current grid to see if anybody's won.
+    // Each sub-array is a list of the squares for a given valid win.
+    // There are only eight, so this seemed cleanest.
+    $wins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
-        foreach ($wins as $win) {
-          if ($grid[$win[0]] == $grid[$win[1]] && $grid[$win[0]] == $grid[$win[2]] && $grid[$win[0]] != 0) {
-            return num2mark($grid[$win[0]]);
-          }
+    // For each possible win, see if all three squares are the same and NOT 0.
+    foreach ($wins as $win) {
+      if ($grid[$win[0]] == $grid[$win[1]] && $grid[$win[0]] == $grid[$win[2]] && $grid[$win[0]] != 0) {
+        // If so, we have a winner, so return the player's mark.
+        return num2mark($grid[$win[0]]);
       }
+    }
   }
 ?>
