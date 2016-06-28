c778cf643b5c0f93409d0b6da04908ea8c5269ce
Susan Tussing <susan@fionavar.net>
2016-06-27 20:31:54 -0500

    Fix CSS for winner

--- index.php
+++ index.php
@@ -45,17 +45,22 @@ HTML;
     display_box($grid, $i, $player);
   }
 
-  $wins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-
-  foreach ($wins as $win) {
-    if ($grid[$win[0]] == $grid[$win[1]] && $grid[$win[0]] == $grid[$win[2]] && $grid[$win[0]] != 0) {
-      $winner = num2mark($grid[$win[0]]);
-      echo "$winner wins!";
-    }
-  }
-
 ?></div>
-<a href="index.php" class="reset">Reset</a>
+
+
+<?php
+    $wins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+    foreach ($wins as $win) {
+      if ($grid[$win[0]] == $grid[$win[1]] && $grid[$win[0]] == $grid[$win[2]] && $grid[$win[0]] != 0) {
+        $winner = num2mark($grid[$win[0]]);
+        echo <<<HTML
+          <div class="winner">$winner wins!</div>
+HTML;
+      }
+  }
+?>
+  <a href="index.php" class="reset">Reset</a>
 
 
 </body>
