041226978ec9379ac5dac8ae50f78d35977e2253
Susan Tussing <susan@fionavar.net>
2016-07-08 14:21:09 -0500

    Fix save/load on completed games

--- controller.php
+++ controller.php
@@ -18,20 +18,21 @@
   // File storage functions
   $save_file = "tttSave.txt";
   function save_to_file() {
-    global $save_file, $grid, $human, $computer;
+    global $save_file, $grid, $human, $computer, $done;
     $fh = fopen($save_file, 'w');
-    $save_data = $grid . $human . $computer;
+    $save_data = $grid . $human . $computer . $done;
     fwrite($fh, $save_data);
   }
 
   function load_from_file() {
-    global $save_file, $grid, $human, $computer;
+    global $save_file, $grid, $human, $computer, $done;
     $fh = fopen($save_file, 'r');
     $save_data = fread($fh, filesize($save_file));
     $grid = substr($save_data, 0, 9);
     $human = $save_data[9];
     $computer = $save_data[10];
+    $done = $save_data[11];
   }
 
-  
+
 ?>
